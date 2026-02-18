5b54181d454eb0cc4149526acaa97e7d4e29c926
steve <steveonjava@gmail.com>
2026-02-18 07:49:26 -0800

    Fix session on first use: ensure session starts correctly on initial login.

--- scripts/ensure-bazzite-desktop-login.sh
+++ scripts/ensure-bazzite-desktop-login.sh
@@ -9,3 +9,14 @@ if [[ ! -f "$BAZZITE_DESKTOP_LOGIN_CONF" ]]; then
     echo "User="
   } | sudo tee "$BAZZITE_DESKTOP_LOGIN_CONF" > /dev/null
 fi
+
+# Configure autologin if Steam has been updated
+if [[ -f $HOME/.local/share/Steam/ubuntu12_32/steamui.so ]]; then
+  {
+    echo "[Users]"
+    echo "RememberLastSession=false" # Don't remember gamescope as the last session (this effectively clears Session, so we also need to reorder this list to make plasma.desktop first)
+    echo "[Autologin]"
+    echo "User=$USER" # Re-enable autologin
+    echo "Session=gamescope-session.desktop"
+  } > "$AUTOLOGIN_CONF"
+fi
